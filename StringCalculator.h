1f3024b0b66c412467d5836b8086934c9d21214d
Ankit Rawat <84120496+AnkitRawat1710@users.noreply.github.com>
2024-09-16 15:31:51 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,18 @@
+#ifndef STRINGCALCULATOR_H
+#define STRINGCALCULATOR_H
+
+#include <string>
+#include <vector>
+
+class StringCalculator {
+public:
+    int add(const std::string& numbers);
+
+private:
+    std::string getDelimiterAndInput(const std::string& numbers, std::string& input);
+    std::vector<std::string> split(const std::string& input, const std::string& delimiter);
+    void checkNegatives(const std::vector<int>& numbers);
+};
+
+#endif // STRINGCALCULATOR_H
 
