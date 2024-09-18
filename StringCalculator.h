d81ed8369e27967954dd1cba5fdb9ebf9705971f
Ankit Rawat <84120496+AnkitRawat1710@users.noreply.github.com>
2024-09-18 23:42:41 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -6,11 +6,13 @@
 
 class StringCalculator {
 public:
-    int add(const std::string& numbers);  // Main method to add numbers from a string.
+    int add(const std::string& input);
 
 private:
-    std::vector<int> parseNumbers(const std::string& input);
     std::string extractDelimiter(const std::string& input);
+    std::vector<std::string> splitNumbers(const std::string& input, const std::string& delimiter); // New method declaration
+    void checkForNegatives(const std::vector<int>& numbers); // New method declaration
+    std::vector<int> parseNumbers(const std::string& input);
 };
 
-#endif  // STRINGCALCULATOR_H
+#endif // STRINGCALCULATOR_H
