75d6cb0ab9e0da6b6ad7e791b63a5ced7697591a
Ankit Rawat <84120496+AnkitRawat1710@users.noreply.github.com>
2024-09-19 00:23:07 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -2,16 +2,16 @@
 #define STRINGCALCULATOR_H
 
 #include <string>
-#include <vector>
-#include <stdexcept>
 
 class StringCalculator
 {
-public:
+  public:
     int checkIfLessThanThousand(int num);
     int validateNonNegative(int num);
+
     std::string adjustDelimiters(std::string input);
     int handleWhitespace(std::string token);
-    int add(std::string input);  // Function name should be `add`
+    int add(std::string input);
 };
+
 #endif // STRINGCALCULATOR_H
