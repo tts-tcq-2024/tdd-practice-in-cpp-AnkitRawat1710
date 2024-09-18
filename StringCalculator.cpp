7faa5ae62341ab16b6a469ded99af945418a2a25
Ankit Rawat <84120496+AnkitRawat1710@users.noreply.github.com>
2024-09-19 00:16:52 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -3,100 +3,81 @@
 #include <iostream>
 #include <cctype>
 #include <algorithm>
-#include <vector>
-#include <stdexcept>
-#include <string>
 
-std::vector<int> negatives;
-int flag = 0;
+namespace {
+    std::vector<int> negativeNumbers;
+    bool hasNegatives = false;
 
-class Exception : public std::runtime_error {
-public:
-    Exception(const std::string& message)
-        : std::runtime_error(message) {}
-};
+    class CustomException : public std::runtime_error {
+    public:
+        CustomException(const std::string& message)
+            : std::runtime_error(message) {}
+    };
 
-void alphabet_error(std::string token)
-{
-    for (char ch : token)
-        if (isalpha(ch))
-        {
-            std::string message = "Alphabets not allowed: ";
-            throw Exception(message);
-        }
-}
-
-void throw_error()
-{
-    std::string message = "Negatives not allowed: ";
-    for (int i = 0; i < negatives.size(); ++i)
-    {
-        message += std::to_string(negatives[i]);
-        if (i < negatives.size() - 1)
-        {
-            message += ", ";
+    void checkForAlphabet(const std::string& token) {
+        for (char ch : token) {
+            if (std::isalpha(ch)) {
+                throw CustomException("Alphabets are not allowed: " + token);
+            }
         }
     }
-    throw Exception(message);
+
+    void validateNegatives() {
+        if (hasNegatives) {
+            std::string message = "Negatives are not allowed: ";
+            for (size_t i = 0; i < negativeNumbers.size(); ++i) {
+                message += std::to_string(negativeNumbers[i]);
+                if (i < negativeNumbers.size() - 1) {
+                    message += ", ";
+                }
+            }
+            throw CustomException(message);
+        }
+    }
 }
 
-int StringCalculator::check_for_less_than_thousand(int num)
-{
-    if (num >= 1000)
+int StringCalculator::checkIfLessThanThousand(int num) {
+    return (num >= 1000) ? 0 : num;
+}
+
+int StringCalculator::validateNonNegative(int num) {
+    if (num < 0) {
+        negativeNumbers.push_back(num);
+        hasNegatives = true;
         return 0;
-    else
-        return num;
-}
-
-int StringCalculator::get_positive_number(int num)
-{
-    int digit = 0;
-    if (num >= 0)
-        digit = check_for_less_than_thousand(num);
-    else
-    {
-        negatives.push_back(num);
-        flag++;
     }
-    return digit;
+    return checkIfLessThanThousand(num);
 }
 
-std::string StringCalculator::Process_String_for_delimiters(std::string input)
-{
-    if (input.substr(0, 2) == "//")
-    {
-        char delimiter = input[2];
+std::string StringCalculator::adjustDelimiters(std::string input) {
+    if (input.rfind("//", 0) == 0) {
+        char customDelimiter = input[2];
         input = input.substr(4);
-        std::replace(input.begin(), input.end(), delimiter, ',');
+        std::replace(input.begin(), input.end(), customDelimiter, ',');
     }
     std::replace(input.begin(), input.end(), '\n', ',');
     return input;
 }
 
-int StringCalculator::exception_handling_for_whitespce(std::string token)
-{
-    if (!token.empty())
-    {  // Check if the token is not empty
-        alphabet_error(token);
-        int num = std::stoi(token);  // Convert to integer
-        return num;
+int StringCalculator::handleWhitespace(std::string token) {
+    if (!token.empty()) {
+        checkForAlphabet(token);
+        return std::stoi(token);
     }
     return 0;
 }
 
-int StringCalculator::add(std::string input)
-{
-    std::string processed_input = Process_String_for_delimiters(input);
-    int sum = 0;
-    std::stringstream ss(processed_input);
+int StringCalculator::add(std::string input) {
+    std::string adjustedInput = adjustDelimiters(input);
+    int total = 0;
+    std::stringstream ss(adjustedInput);
     std::string token;
-    while (std::getline(ss, token, ','))
-    {
-        int num = exception_handling_for_whitespce(token);
-        int addition = get_positive_number(num);
-        sum += addition;
+
+    while (std::getline(ss, token, ',')) {
+        int number = handleWhitespace(token);
+        total += validateNonNegative(number);
     }
-    if (flag > 0)
-        throw_error();
-    return sum;
+
+    validateNegatives();
+    return total;
 }
